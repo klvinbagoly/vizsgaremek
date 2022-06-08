a18420da59ad791095c214ddbdc51a5eb56dc2df
klvinbagoly <kbalazsk1@gmail.com>
2022-06-08 09:46:01 +0200

    Buttons on the form

--- frontend/src/app/form-dialog/form-dialog.module.ts
+++ frontend/src/app/form-dialog/form-dialog.module.ts
@@ -1,4 +1,3 @@
-import { MatDialogModule } from '@angular/material/dialog';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArtistEditorComponent } from './form/artist-editor/artist-editor.component';
@@ -6,6 +5,11 @@ import { AlbumEditorComponent } from './form/album-editor/album-editor.component
 import { TagEditorComponent } from './form/tag-editor/tag-editor.component';
 import { FormInputComponent } from './form/form-input/form-input.component';
 import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 
 
 
@@ -20,7 +24,10 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    MatDialogModule
+    MatDialogModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule
   ],
   exports: [
     ArtistEditorComponent,
