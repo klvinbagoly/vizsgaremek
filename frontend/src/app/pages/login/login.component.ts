6031ac13b514978d9e2e814b9ab16ff66ccd4bc2
klvinbagoly <kbalazsk1@gmail.com>
2022-06-17 00:23:16 +0200

    authService login, logout, refresh methods

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/model/user';
+import { AuthService } from 'src/app/service/auth.service';
+
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private auth: AuthService
+  ) { }
 
   ngOnInit(): void {
+
   }
 
 }
--- frontend/src/app/service/auth.service.ts
+++ frontend/src/app/service/auth.service.ts
@@ -1,19 +1,89 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject, of, switchMap } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { User } from '../model/user';
+
+export interface ILoginRequest {
+  email: string,
+  password: string
+}
+
+export interface IAuthResponse {
+  accessToken: string,
+  refreshToken: string,
+  user: User
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  loginUrl = environment.apiUrl + 'login'
+  logoutUrl = environment.apiUrl + 'logout'
+  refreshUrl = environment.apiUrl + 'refresh'
+
+  lastUser: BehaviorSubject<User | null> = new BehaviorSubject<User | null>(null)
+  lastAccessToken: BehaviorSubject<string> = new BehaviorSubject<string>('')
+  lastRefreshToken: BehaviorSubject<string> = new BehaviorSubject<string>('')
 
 
   constructor(
-    private http: HttpClient
-  ) { }
+    private http: HttpClient,
+    private router: Router
+  ) {
+    const user = sessionStorage.getItem('user')
+    if (user) {
+      this.lastUser.next(JSON.parse(user))
+    }
+  }
 
-  login(user: User) {
+  login(user: ILoginRequest) {
+    return this.http.post<IAuthResponse>(this.loginUrl, user)
+      .pipe(switchMap(response => {
+        if (response.accessToken) {
+          this.lastUser.next(response.user)
+          this.lastAccessToken.next(response.accessToken)
+          this.lastRefreshToken.next(response.refreshToken)
+          sessionStorage.setItem('user', JSON.stringify(response.user))
+          return of(response.user)
+        } else {
+          this.lastUser.next(null)
+          sessionStorage.removeItem('user')
+          return of(null)
+        }
+      }))
+  }
+
+  logout() {
+    const refreshToken = this.lastRefreshToken.getValue()
+    this.http.post(this.logoutUrl, { refreshToken }).subscribe(
+      response => {
+        console.log(response)
+        this.lastUser.next(null)
+        this.lastAccessToken.next('')
+        this.lastRefreshToken.next('')
+        sessionStorage.removeItem('user')
+        this.router.navigate(['/login'])
+      }
+    )
+
 
   }
+
+  refresh() {
+    const refreshToken = this.lastRefreshToken.getValue()
+    this.http.post<{ accessToken: string }>(this.refreshUrl, { refreshToken })
+      .subscribe({
+        next: response => {
+          if (response.accessToken) {
+            this.lastAccessToken.next(response.accessToken)
+          }
+        }
+      })
+  }
 }
+
+
